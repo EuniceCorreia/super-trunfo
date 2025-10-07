9b3c1931bdc05aab391828f82bd6c5b8dcae2130
eunice.193523 <nice.correia@hotmail.com>
2025-10-06 21:51:01 -0300

    Alterações

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native/types_generated/index';
+import { drivers } from './data/drivers';
 
 export default function App() {
+  //var driver = drivers[3];
+  const[driver, setDriver] = useState(drivers[0])
+  
+  const changeDriver = () =>
+  {
+    setDriver(drivers[5]);
+      console.log(driver);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>EUNICE</Text>
+      <Image 
+      style ={styles.image}
+      source={{uri: driver.image}}/>
+      <Button 
+      title = 'Trocar piloto'
+      onPress={changeDriver}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginBottom: 16
+  }
 });
